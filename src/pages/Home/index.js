d1c7b545df7999d37037064a9504b243bf371d91
Wesley Eugênio <wesleyeugenio3@gmail.com>
2022-09-29 12:14:29 -0300

    seção de boas-vindas e botões em andamento

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,11 +1,24 @@
 import "./styles.css";
 import "../../global.css";
 import Header from "../../components/Header";
+import Looper from "../../assets/looper-bg.png";
 
 const Home = () => {
   return (
-    <div className="container">
+    <div className="home">
       <Header />
+      <section className="welcome">
+        <div className="welcome__text">
+          <span className="welcome__introduction">Olá, eu sou</span>
+          <span className="welcome__name">Wesley Eugênio</span>
+          <span className="welcome__profession">Desenvolvedor Full Stack.</span>
+        </div>
+        <div className="welcome__buttons">
+          <button className="btn--linkedin"></button>
+          <button className="btn--gitHub"></button>
+        </div>
+        <img className="welcome__img" src={Looper} alt="Imagem background" />
+      </section>
     </div>
   );
 };
