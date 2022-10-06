b87d3bb58a60683765ae13d5273dfd59ab69cf57
Wesley Eugênio <wesleyeugenio3@gmail.com>
2022-10-05 23:10:56 -0300

    responsividade do header feito

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,28 +1,37 @@
 import "./styles.css";
 import MenuHamburger from "../../assets/menu-hamburger.png"
+import { useState } from "react";
 
 const Header = () => {
+  const [showMenu, setShowMenu] = useState(false)
+
+  const handleClickLink = () => {
+    if (showMenu) {
+      setShowMenu(false)
+    }
+  }
+
   return (
-    <header className="header">
+    <header className={`${showMenu ? 'header--mobile' : 'header'}`}>
       <nav className="header__nav">
         <a className="header__logo" href="/">
           Portfólio
         </a>
         <ul className="header__links">
-          <a className="header__text" href="/">
+          <a className="header__text" href="/" onClick={()=> handleClickLink()}>
             Home
           </a>
-          <a className="header__text" href="/">
+          <a className="header__text" href="/" onClick={()=> handleClickLink()}>
             Sobre mim
           </a>
-          <a className="header__text" href="/">
+          <a className="header__text" href="/" onClick={()=> handleClickLink()}>
             Experiência
           </a>
-          <a className="header__text" href="/">
+          <a className="header__text" href="/" onClick={()=> handleClickLink()}>
             Projetos
           </a>
         </ul>
-        <img className="header__img" src={MenuHamburger} alt='Menu hamburger' />
+        <img className="header__img" src={MenuHamburger} alt='Menu hamburger' onClick={() => setShowMenu(!showMenu)} />
       </nav>
     </header>
   );
