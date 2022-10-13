4ba1bce6828d236fdc7b4876c0ec0cf69b4a34c3
Wesley Eugênio <wesleyeugenio3@gmail.com>
2022-10-13 16:42:49 -0300

    responsividade no botão feita

---
+++ src/components/Button/index.jsx
@@ -0,0 +1,14 @@
+import "./styles.css";
+
+const Button = ({ text, icon, color }) => {
+  return (
+    <button
+      className={`${color === "purple" ? "button--purple" : "button--blue"} `}
+    >
+      <img className='button__icon' src={icon} alt='Ícone do botão' />
+      {text}
+    </button>
+  );
+};
+
+export default Button;
