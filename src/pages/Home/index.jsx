2715142fa175cc3f0d74b103c19d54a7dfeb2f70
Wesley Eugênio <wesleyeugenio3@gmail.com>
2022-10-18 16:03:54 -0300

    adição do footer, projetos nova organização

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -9,7 +9,10 @@ import GitHub from "../../assets/github.png";
 import YouTube from "../../assets/youtube.png";
 import Profile from "../../assets/profile.png";
 import Twitter from "../../assets/twitter.png";
+import Kontacts from "../../assets/kontacts.png";
 import CompanyButton from "../../components/CompanyButton";
+import Footer from "../../components/Footer";
+import Card from "../../components/Card";
 
 const Home = () => {
   return (
@@ -22,29 +25,11 @@ const Home = () => {
           <span className='welcome__profession'>Desenvolvedor Full Stack.</span>
         </div>
         <div className='welcome__buttons'>
-          <Button text='GitHub' icon={GitHub} color='purple' />
-          <Button text='LinkedIn' icon={LinkedIn} color='blue' />
+          <Button text='Entre em Contato' icon={WhatsApp} color='blue' />
         </div>
         <img className='welcome__img' src={Looper} alt='Imagem background' />
       </section>
-      <section className='about-me container section' id='about-me'>
-        <img className='about-me__img' src={Profile} alt='Perfil' />
-        <div className='about-me__information'>
-          <h2 className='about-me__title'>Sobre mim</h2>
-          <h3 className='about-me__subtitle'>Bahia, Brasil</h3>
-          <p className='about-me__paragraph'>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent id
-            egestas dui. Nullam finibus aliquam enim quis faucibus. Aenean ac
-            commodo dolor, nec bibendum velit.
-          </p>
-          <div className='about-me__social-media'>
-            <img className='about-me__media' src={YouTube} alt='YouTube' />
-            <img className='about-me__media' src={Twitter} alt='Twitter' />
-          </div>
-          <Button text='Contato' icon={WhatsApp} color='blue' />
-        </div>
-      </section>
-      <section className='experience container section'>
+      <section className='experience container section' id='experience'>
         <h2 className='experience__title'>Experiência</h2>
         <div className='experience__data'>
           <div className='experience__companies'>
@@ -57,14 +42,42 @@ const Home = () => {
             </div>
             <span className='experience__company-name'>Digital Seller</span>
             <p className='experience__paragraph'>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque
-              eu purus risus. Ut rutrum sollicitudin purus in accumsan. Proin at
-              mattis metus. Nullam sit amet mauris condimentum, volutpat augue
-              in, mattis urna.
+              Atuava no desenvolvimento e manutenção das páginas da empresa,
+              utilizando HTML, CSS, PHP, Git e GitHub. Desenvolvi e aprimorei
+              habilidades como trabalho em equipe, gestão das atividades e
+              comunicação.
             </p>
           </div>
         </div>
       </section>
+      <section className='projects container section' id='projects'>
+        <h2 className='projects__title'>Projetos</h2>
+        <div className='projects__cards'>
+          <Card img={Kontacts} />
+          <Card img={Kontacts} />
+        </div>
+      </section>
+      <section className='about-me container section' id='about-me'>
+        <img className='about-me__img' src={Profile} alt='Perfil' />
+        <div className='about-me__information'>
+          <h2 className='about-me__title'>Sobre mim</h2>
+          <h3 className='about-me__subtitle'>Bahia, Brasil</h3>
+          <p className='about-me__paragraph'>
+            Sou Desenvolvedor Full Stack com formação técnica, experiência e
+            domino as seguintes tecnologias: HTML, CSS, JavaScript, ReactJS,
+            Material UI, NodeJS, Git, GitHub e deploy. Tenho como missão
+            resolver problemas reais por meio da tecnologia e por isso me
+            desafio constantemente em busca de conhecimento.
+          </p>
+          <div className='about-me__social-media'>
+            <img className='about-me__media' src={YouTube} alt='YouTube' />
+            <img className='about-me__media' src={Twitter} alt='Twitter' />
+            <img className='about-me__media' src={LinkedIn} alt='LinkedIn' />
+            <img className='about-me__media' src={GitHub} alt='GitHub' />
+          </div>
+        </div>
+      </section>
+      <Footer />
     </div>
   );
 };
