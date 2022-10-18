2a7b84993c0bbbacf1d400fba7f5e16a3213d104
Wesley Eugênio <wesleyeugenio3@gmail.com>
2022-10-18 16:02:13 -0300

    criação do footer

---
+++ src/components/Footer/index.jsx
@@ -0,0 +1,11 @@
+import "./styles.css";
+
+const Footer = () => {
+  return (
+    <div className='footer container'>
+      <span className='footer__text'>@ 2022 - Wesley Costa</span>
+    </div>
+  );
+};
+
+export default Footer;
