a2b2c94e698bce2257acec5ae2d59a57cc3292e2
Wesley Eugênio <wesleyeugenio3@gmail.com>
2022-10-29 19:20:59 -0300

    criação da lista de projetos

---
+++ src/projects.js
@@ -0,0 +1,11 @@
+import Kontacts from "./assets/kontacts.png";
+
+export const projects = [
+  {
+    title: "Kontacts",
+    text: "Se trata de uma agenda pessoal de contatos construída com React JS e Material UI que se comunica com uma API.",
+    link: "http://reactjs-mui-kontacts.vercel.app/",
+    img: [Kontacts],
+    technologies: ["HTML", "CSS", "JavaScript", "ReactJS", "Material UI"],
+  },
+];
