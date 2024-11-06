3306301e4e8461a98fa62a92478a14634f86ec36
Wesley Costa <wesleyeugenio3@gmail.com>
2024-11-06 17:12:23 -0300

    feat: add SASS in experience section

--- src/companies.js
+++ src/companies.js
@@ -1,23 +1,27 @@
-export const erural = {
-  occupation: "Desenvolvedor Full-Stack Jr",
-  name: "erural",
-  period: "Dez 2022 - Set 2024",
-  description:
-    "Atuava desenvolvendo novas funcionalidades e dando manutenção ao projeto da empresa tanto no frontend como também no backend, e para isso utilizo meus conhecimentos em: HTML, CSS, JavaScript, Ruby, VueJS, Ruby on Rails, Docker, versionamento com Git e GitHub.",
-};
+const companies = [
+  {
+    occupation: "Desenvolvedor Full-Stack Jr",
+    name: "erural",
+    period: "Dez 2022 - Set 2024",
+    description:
+      "Atuava desenvolvendo novas funcionalidades e dando manutenção ao projeto da empresa tanto no frontend como também no backend, e para isso utilizo meus conhecimentos em: HTML, CSS, JavaScript, Ruby, VueJS, Ruby on Rails, Docker, versionamento com Git e GitHub.",
+  },
 
-export const cubos_academy = {
-  occupation: "Desenvolvedor Full-Stack",
-  name: "Cubos Academy",
-  period: "Nov 2022 - Dez 2022",
-  description:
-    "Atuava no desenvolvimento de novas funcionalidades com: ReactJS, Typescript e SASS para o frontend. Mas também atuei no backend com NodeJS e Typescript. Por meio dessa experiencia aprimorei meu trabalho em equipe e minha gestão de entregas.",
-};
+  {
+    occupation: "Desenvolvedor Full-Stack",
+    name: "Cubos Academy",
+    period: "Nov 2022 - Dez 2022",
+    description:
+      "Atuava no desenvolvimento de novas funcionalidades com: ReactJS, Typescript e SASS para o frontend. Mas também atuei no backend com NodeJS e Typescript. Por meio dessa experiencia aprimorei meu trabalho em equipe e minha gestão de entregas.",
+  },
 
-export const digital_seller = {
-  occupation: "Estagiário Front-End",
-  period: "Nov 2021 - Dez 2021",
-  name: "Digital Seller",
-  description:
-    "Atuava no desenvolvimento e manutenção das páginas da empresa, utilizando HTML, CSS, PHP, Git e GitHub. Desenvolvi e aprimorei habilidades como: trabalho em equipe, gestão das atividades e comunicação.",
-};
+  {
+    occupation: "Estagiário Front-End",
+    period: "Nov 2021 - Dez 2021",
+    name: "Digital Seller",
+    description:
+      "Atuava no desenvolvimento e manutenção das páginas da empresa, utilizando HTML, CSS, PHP, Git e GitHub. Desenvolvi e aprimorei habilidades como: trabalho em equipe, gestão das atividades e comunicação.",
+  },
+];
+
+export default companies;
--- src/components/Experience/index.jsx
+++ src/components/Experience/index.jsx
@@ -1,82 +1,42 @@
-import "./styles.css";
+import "./styles.scss";
 import { useState } from "react";
-import { erural, cubos_academy, digital_seller } from "../../companies";
+import companies from "../../companies";
 
 const Experience = () => {
-  const [company, setCompany] = useState("erural");
+  const [companyState, setCompanyState] = useState("erural");
 
   return (
     <section className='experience container section' id='experience'>
       <h2 className='experience__title'>Experiências</h2>
       <div className='experience__data'>
         <div className='experience__companies'>
-          <div className='company-button--border'>
-            <button
-              className={`company-button${
-                company === "erural" ? "--active" : ""
-              }`}
-              onClick={() => setCompany("erural")}
-            >
-              erural
-            </button>
-            <button
-              className={`company-button${
-                company === "Cubos Academy" ? "--active" : ""
-              }`}
-              onClick={() => setCompany("Cubos Academy")}
-            >
-              Cubos Academy
-            </button>
-            <button
-              className={`company-button${
-                company === "Digital Seller" ? "--active" : ""
-              }`}
-              onClick={() => setCompany("Digital Seller")}
-            >
-              Digital Seller
-            </button>
+          <div>
+            {companies.map((company) => (
+              <button
+                className={`company-button${
+                  companyState === company.name ? "--active" : ""
+                }`}
+                onClick={() => setCompanyState(company.name)}
+              >
+                {company.name}
+              </button>
+            ))}
           </div>
         </div>
-        {company === erural.name && (
-          <div className='experience__description'>
-            <div className='experience__header'>
-              <h2 className='experience__office'>{erural.occupation}</h2>
-              <span className='experience__period'>{erural.period}</span>
+        {companies.map((company) => {
+          return company.name === companyState ? (
+            <div className='experience__description'>
+              <div className='experience__header'>
+                <h2 className='experience__office'>{company.occupation}</h2>
+                <span className='experience__period'>{company.period}</span>
+              </div>
+              <span className='experience__company-name'>{company.name}</span>
+              <p className='experience__paragraph'>{company.description}</p>
             </div>
-            <span className='experience__company-name'>{erural.name}</span>
-            <p className='experience__paragraph'>{erural.description}</p>
-          </div>
-        )}
-        {company === cubos_academy.name && (
-          <div className='experience__description'>
-            <div className='experience__header'>
-              <h2 className='experience__office'>{cubos_academy.occupation}</h2>
-              <span className='experience__period'>{cubos_academy.period}</span>
-            </div>
-            <span className='experience__company-name'>
-              {cubos_academy.name}
-            </span>
-            <p className='experience__paragraph'>{cubos_academy.description}</p>
-          </div>
-        )}
-        {company === digital_seller.name && (
-          <div className='experience__description'>
-            <div className='experience__header'>
-              <h2 className='experience__office'>
-                {digital_seller.occupation}
-              </h2>
-              <span className='experience__period'>
-                {digital_seller.period}
-              </span>
-            </div>
-            <span className='experience__company-name'>
-              {digital_seller.name}
-            </span>
-            <p className='experience__paragraph'>
-              {digital_seller.description}
-            </p>
-          </div>
-        )}
+          ) : (
+            ""
+          );
+        })}
       </div>
     </section>
   );
