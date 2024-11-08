62b0b315b4d491cf813a56c13f851a12f9360ed6
Wesley Costa <wesleyeugenio3@gmail.com>
2024-11-08 12:02:44 -0300

    feat: add SASS in index file

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "./index.css";
+
 import Home from "./pages/Home/index";
+import "./index.scss";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
